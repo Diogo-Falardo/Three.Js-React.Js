4e82658231d130df9ee5c9cd86dc3f1910db92a1
Diogo Falardo <diogofalardo04@gmail.com>
2025-01-19 18:56:23 +0000

    CDN

--- index.js
+++ index.js
@@ -3,38 +3,54 @@ import * as THREE from "three";
 const w = window.innerWidth;
 const h = window.innerHeight;
 
+
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-// Settings For the 
+// Settings For the Object
+// Diferent objects have diferent assets
 
-const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
+const radius = 4;  
+const detail = 1;
+
+// BoxGeometry -> Cube
+const geometry = new THREE.DodecahedronGeometry(radius, detail);
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#242424'});
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-const light = new THREE.DirectionalLight(0x9CDBA6, 10);
+// Light 
+// Color of the light
+// Intensity of the light
+const light = new THREE.DirectionalLight('#dd2512', 100);
 light.position.set(1, 1, 1);
 scene.add(light);
 
-const fov = 75;
-const aspect = w / h;
-const near = 0.1;
-const far = 10;
+const fov = 100; // Distance
+const aspect = w / h; // Aspect Ratio
+const near = 1; // If object is near that position it removes it from the camera
+const far = 10; // If object is far from that position it removes it from the camera
 
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+// Camera position 
+// lower the number closer to the object 
+// if too low might impact and we cant see the object
+camera.position.z = 10;
 
 // Render loop
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01; // Example animation
+    cube.rotation.x += 0.001; // Example animation
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
 animate();
+
+
+
+
